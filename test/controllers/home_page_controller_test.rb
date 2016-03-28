31bf521f7035b71932825757b287af5dd48342c3
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-03-27 23:40:31 -0400

    Add full_title helper

--- test/controllers/home_page_controller_test.rb
+++ test/controllers/home_page_controller_test.rb
@@ -1,24 +1,29 @@
 require 'test_helper'
 
 class HomePageControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Acxplore"
+  end
+
   test "should get home" do
     get :home
-    assert_select "title", "Home | Acxplore"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get :about
-    assert_select "title", "About | Acxplore"
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
     get :contact
-    assert_select "title", "Contact | Acxplore"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
   test "should get help" do
     get :help
-    assert_select "title", "Help | Acxplore"
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
