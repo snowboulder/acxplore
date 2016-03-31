55511679f7de68cc8824e8cda896d3c0b29fcc56
Kevin Chao <kevin.chao@mail.utoronto.ca>
2016-03-31 14:33:57 -0400

    Make basic User signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'contact'   => 'home_page#contact'
   get 'help'      => 'home_page#help'
   get 'signup'    => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
